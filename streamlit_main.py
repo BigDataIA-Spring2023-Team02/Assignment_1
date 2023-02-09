7e4d856a7ce300675f8128ed50c2de0cc137428b
ajinabraham123 <lnu.aj@northeastern.edu>
2023-02-09 17:01:21 -0500

    Updated the Ui and fixed the bugs plus reduced the size of the code

--- streamlit_main.py
+++ streamlit_main.py
@@ -6,10 +6,7 @@ from file_url_generate import *
 from aws_main import *
 from noaa_scrape_data import *
 import pandas as pd
-
-
 from sqlite import *
-
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 logging.basicConfig(
@@ -20,131 +17,114 @@ logging.basicConfig(
 
 aws_main = AWS_Main()
 noaa_scrape = Scrape_Data()
+goes_sqlite = GoesSqlite()
+nexrad_sqlite = NexradSqlite()
+geos_data = goes_sqlite.main()
+nexrad_data = nexrad_sqlite.main()
 
-geos_data = noaa_scrape.geos18_data()
+def geos_search_field(satellite_input):
+    st.markdown("<h3 style='text-align: center;'>Search By Field</h1>", unsafe_allow_html=True)
+    
+    st.write(geos_data)
 
-goessqlite = GoesSqlite()
-geos_data = goessqlite.main()
-nexradsqlite = NexradSqlite()
-nexrad_data = nexradsqlite.main()
-df = pd.DataFrame(columns=['col1'])
+    product_input = st.selectbox('Select Product Name', geos_data['Product_Name'].unique())
+    if product_input:
+        years = geos_data[geos_data['Product_Name'] == product_input]['Year'].unique()
+        year_input = st.selectbox('Select Year', years)
+        if year_input:
+            day = geos_data[geos_data['Year'] == year_input]['Day'].unique()
+            day_input = st.selectbox('Select Day', day)
+            if day_input:
+                hour = geos_data[geos_data['Day'] == day_input]['Hour'].unique()
+                hour_input = st.selectbox('Select Hour', hour)
+                if hour_input:
+                    files_list = aws_main.list_files_in_noaa_goes18_bucket(product_input, year_input, day_input, hour_input)
+                    file_input = st.selectbox('Select File Name', files_list)
 
-# write the initial dataframe to the CSVUs file
-df.to_csv("/Users/siddhisawant/Downloads/BigDataSys/GitHub/Assignment_1/meet.csv", index=False)
+                    if st.button('Download to S3 Bucket'):
+                        selected_file = product_input + '/' + year_input + '/' + day_input + '/' + hour_input + '/' + file_input
+                        url_s3, url_noaa = aws_main.copy_file_to_user_bucket(selected_file, file_input, satellite_input)
+                        # st.write('File Link in S3 Bucket !!!\n', url_s3)
+                        # st.write('File Link in NOAA Bucket !!!\n', url_noaa)
 
+def geos_search_filename(satellite_input):
+    st.markdown("<h3 style='text-align: center;'>Search By Filename</h1>", unsafe_allow_html=True)
+    file_name = st.text_input('NOAA GEOS-18 Filename',)
+    
+    try:
+        if st.button('Download to S3 Bucket'):
+            url, selected_file_key = goes_18_link_generation(file_name)
+            url_s3, url_noaa = aws_main.copy_file_to_user_bucket(selected_file_key, file_name, satellite_input)
+            st.write('File Link in S3 Bucket !!!\n', url_s3)
+            st.write('File Link in NOAA Bucket !!!\n', url_noaa)
+        
+    except ValueError:
+        st.error('Oops! Unable to Generate')
 
+def nexrad_search_field(satellite_input):
+    
+    st.markdown("<h3 style='text-align: center;'>Search By Field</h1>", unsafe_allow_html=True)
+    
+    year_input = st.selectbox('Select Year', nexrad_data['Year'].unique())
+    if year_input:
+        months = nexrad_data[nexrad_data['Year'] == year_input]['Month'].unique()
+        month_input = st.selectbox('Select Month', months)
+        if month_input:
+            day = nexrad_data[nexrad_data['Month'] == month_input]['Day'].unique()
+            day_input = st.selectbox('Select Day', day)
+            if day_input:
+                station_code = nexrad_data[nexrad_data['Day'] == day_input]['NexRad Station Code'].unique()
+                station_code_input = st.selectbox('NexRad Station Code', station_code)
+                if station_code_input:
+                    files_list = aws_main.list_files_in_noaa_nexrad_bucket(year_input, month_input, day_input, station_code_input)
+                    file_input = st.selectbox('Select File Name', files_list)
 
-
+                    if st.button('Download to S3 Bucket'):
+                        selected_file = year_input + '/' + month_input + '/' + day_input + '/' + station_code_input + '/' + file_input
+                        url_s3, url_noaa = aws_main.copy_file_to_user_bucket(selected_file, file_input, satellite_input)
+                        
+def nexrad_search_filename(satellite_input):
+    st.markdown("<h3 style='text-align: center;'>Search By Filename</h1>", unsafe_allow_html=True)
+    file_name = st.text_input('NOAA GEOS-18 Filename',)
+    
+    try:
+        if st.button('Download to S3 Bucket'):
+            url, selected_file_key = nexrad_link_generation(file_name)
+            url_s3, url_noaa = aws_main.copy_file_to_user_bucket(selected_file_key, file_name, satellite_input)
+            
+    except ValueError:
+        st.error('Oops! Unable to Generate')
 
 def geos_dataset():
     option = st.selectbox('Select the option to search file', ('--Select Search Type--', 'Search By Field', 'Search By Filename'))
+    satellite_input = 'geos18'
 
     if option == '--Select Search Type--':
         st.error('Select an input field')
     elif option == 'Search By Field':
-        st.markdown("<h3 style='text-align: center;'>Search By Field</h1>", unsafe_allow_html=True)
-        
-        product_input = st.selectbox('Select Product Name', geos_data['Product_Name'].unique())
-        if product_input:
-            years = geos_data[geos_data['Product_Name'] == product_input]['Year'].unique()
-            year_input = st.selectbox('Select Year', years)
-            if year_input:
-                day = geos_data[geos_data['Year'] == year_input]['Day'].unique()
-                day_input = st.selectbox('Select Day', day)
-                if day_input:
-                    hour = geos_data[geos_data['Day'] == day_input]['Hour'].unique()
-                    hour_input = st.selectbox('Select Hour', hour)
-                    if hour_input:
-                        files_list = aws_main.list_files_in_noaa_goes18_bucket(product_input, year_input, day_input, hour_input)
-                        file_input = st.selectbox('Select File Name', files_list)
-
-                        if st.button('Generate URL Link'):
-                            url = goes18_filename_link_generation(product_input, year_input, day_input, hour_input, file_input)
-                            
-                            bucket_name = "damg-7245-projects"
-                            file_name = "/Users/ajinabraham/Documents/GitHub/BigData/Assignment_1/meet.csv"
-
-                            client = boto3.client('s3',region_name = 'us-east-1',aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),aws_secret_access_key = os.environ.get('AWS_SECRET_KEY'))
-
-                            #client.download_file(bucket_name, file_name, "meet.txt") 
-                            #with open("/Users/ajinabraham/Documents/GitHub/BigData/Assignment_1/meet.xlsx","a") as file:file.write(url)
-                           # aws_main.list_files_in_user_bucket()
-                            st.write('Generated URL Link:\n', url)
-                        
-                        if st.button('Download to S3 Bucket'):
-                            st.write("Inside Download Button")
-                            # upload_file_to_user_bucket()
-                            # url_3, url_noaa = upload_file_to_user_bucket()
-                            # st.write('File Link in S3 Bucket !!!\n', url_s3)
-                            # st.write('File Link in NOAA Bucket !!!\n', url_noaa)
-
+        geos_search_field(satellite_input)
+    
     elif option == 'Search By Filename':
-        st.markdown("<h3 style='text-align: center;'>Search By Filename</h1>", unsafe_allow_html=True)
-        file_name = st.text_input('NOAA GEOS-18 Filename',)
-        
-        try:
-            if st.button('Generate URL Link'):
-                url = goes_18_link_generation(file_name)
-                st.write('Generated URL Link:\n', url)
-        except ValueError:
-            st.error('Oops! Unable to Generate')
-
+        geos_search_filename(satellite_input)
 
 def nexrad_dataset():
     option = st.selectbox('Select the option to search file', ('--Select Search Type--', 'Search By Field', 'Search By Filename'))
+    satellite_input = 'nexrad'
 
     if option == '--Select Search Type--':
         st.error('Select an input field')
     elif option == 'Search By Field':
-        st.markdown("<h3 style='text-align: center;'>Search By Field</h1>", unsafe_allow_html=True)
-        
-        product_input = st.selectbox('Select Year', nexrad_data['Year'].unique())
-        if product_input:
-            month = nexrad_data[nexrad_data['Year'] == product_input]['Month'].unique()
-            month_input = st.selectbox('Select Month', month)
-            if month_input:
-                day = nexrad_data[nexrad_data['Month'] == month_input]['Day'].unique()
-                day_input = st.selectbox('Select Day', day)
-                if day_input:
-                    station = nexrad_data[nexrad_data['Day'] == day_input]['NexRad Station Code'].unique()
-                    station_input = st.selectbox('Select NexRad Station Code', station)
-                    if station_input:
-                        files_list = aws_main.list_files_in_noaa_nexrad_bucket(product_input, month_input, day_input, station_input)
-                        file_input = st.selectbox('Select File Name', files_list)
-
-                        if st.button('Generate URL Link'):
-                            url = nexrad_filename_link_generation(product_input, month_input, day_input, file_input, station_input)
-                            
-                            bucket_name = "damg-7245-projects"
-                            file_name = "/Users/ajinabraham/Documents/GitHub/BigData/Assignment_1/meet.csv"
-
-                            client = boto3.client('s3',region_name = 'us-east-1',aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),aws_secret_access_key = os.environ.get('AWS_SECRET_KEY'))
-
-                                #client.download_file(bucket_name, file_name, "meet.txt") 
-                                #with open("/Users/ajinabraham/Documents/GitHub/BigData/Assignment_1/meet.xlsx","a") as file:file.write(url)
-                            # aws_main.list_files_in_user_bucket()
-                        if st.button('Download to S3 Bucket'):
-                            st.write("Inside Download Button")
-                            # upload_file_to_user_bucket()
-                            # url_3, url_noaa = upload_file_to_user_bucket()
-                            # st.write('File Link in S3 Bucket !!!\n', url_s3)
-                            # st.write('File Link in NOAA Bucket !!!\n', url_noaa)
-
+        nexrad_search_field(satellite_input)
+    
     elif option == 'Search By Filename':
-        st.markdown("<h3 style='text-align: center;'>Search By Filename</h1>", unsafe_allow_html=True)
-        file_name = st.text_input('NEXRAD Filename',)
-        
-        try:
-            if st.button('Generate URL Link'):
-                url = noaa_scrape.nexrad_link_generatio(file_name)
-                st.write('Generated URL Link:\n', url)
-        except ValueError:
-            st.error('Oops! Unable to Generate')
+        nexrad_search_filename(satellite_input)
 
+def nexrad_mapdata():
 
+    return
 
 def main():
-    page = st.sidebar.selectbox("Choose a page", ["--Select Data--", "GEOS Data", "NexRad Data"])
+    page = st.sidebar.selectbox("Choose a page", ["--Select Data--", "GEOS Data", "NexRad Data", "NexRad Map Locations"])
     st.markdown("<h1 style='text-align: center;'>Geospatial Data Exploration Tool</h1>", unsafe_allow_html=True)
 
     if page == "--Select Data--":
@@ -157,6 +137,10 @@ def main():
     elif page == "NexRad Data":
         st.markdown("<h2 style='text-align: center;'>Data Exploration of the NexRad dataset</h1>", unsafe_allow_html=True)
         nexrad_dataset()
+    
+    elif page == "NexRad Map Locations":
+        st.markdown("<h2 style='text-align: center;'>Data Exploration of the NexRad dataset</h1>", unsafe_allow_html=True)
+        nexrad_mapdata()
     
     return
 
