24df19005905a49befbe71b8527ca72f1d599190
Meet Doshi <doshi.mee@northeastern.edu>
2023-02-09 13:31:49 -0500

    added code for copying in user bucket

--- aws_main.py
+++ aws_main.py
@@ -2,10 +2,11 @@ import os
 import boto3
 import logging
 from dotenv import load_dotenv
+import streamlit as st
 
+@st.cache
 class AWS_Main:
     def __init__(self):
-    
         load_dotenv ()
 
         LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
@@ -20,30 +21,30 @@ class AWS_Main:
                                 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
                                 aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
                                 )
-
+        
+        self.s3resource = boto3.resource('s3',
+                                region_name = 'us-east-1',
+                                aws_access_key_id = os.environ.get('AWS_ACCESS_KEY'),
+                                aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
+                                )
+        
         self.geos_bucket_name = "noaa-goes18"
         self.nexrad_bucket_name = "noaa-nexrad-level2"
 
     def list_files_in_user_bucket(self):
-        logging.debug("fetching objects in user s3 bucket")
         my_bucket = self.s3client.list_objects_v2(Bucket = os.environ.get('USER_BUCKET_NAME')).get('Contents')
-        logging.info("Printing files from user S3 bucket")
         file_list = []
         for file in my_bucket:
             file_list.append(file['Key'])
         return file_list
 
     def list_files_in_noaa_goes18_bucket(self, product, year, day, hour):
-        logging.debug("fetching objects in noaa goes18 s3 bucket")
-        # product_name, year_input, day_input, hour_input
         year_input = year
         day_input = day
         hour_input = hour
-        product_name = product  #'ABI-L1b-RadC'
+        product_name = product
         prefix = product_name + '/' + year_input + '/' + day_input + '/' + hour_input + '/'
         geos_bucket = self.s3client.list_objects(Bucket = self.geos_bucket_name, Prefix = prefix).get('Contents')
-        logging.info("Printing files from GOES18 S3 bucket")
-        print("Files available to download from the selected location:")
         file_list = []
         for objects in geos_bucket:
             file_path = objects['Key']
@@ -51,37 +52,13 @@ class AWS_Main:
             file_list.append(file_path[-1])
         return file_list
 
-
-    def list_files_in_noaa_nexrad_bucket(self):
-        logging.debug("fetching objects in noaa nexrad s3 bucket")
-        # year_input, month_input, day_input, station_code
-        year_input = '2022'
-        month_input = '02'
-        day_input = '05'
-        station_code = 'KABX'
-        prefix = year_input + '/' + month_input + '/' + day_input + '/' + station_code + '/'
-        geos_bucket = self.s3client.list_objects(Bucket = self.nexrad_bucket_name, Prefix = prefix).get('Contents')
-        logging.info("Printing files from NEXRAD bucket")
-        print("Files available to download from the selected location:")
-        file_list = []
-        for objects in geos_bucket:
-            file_path = objects['Key']
-            file_path = file_path.split('/')
-            file_list.append(file_path[-1])
-        return file_list
-
-
-    def list_files_in_noaa_nexrad_bucket(self,year,month,day,station):
-        logging.debug("fetching objects in noaa nexrad s3 bucket")
-        # year_input, month_input, day_input, station_code
+    def list_files_in_noaa_nexrad_bucket(self, year, month, day, station):
         year_input = year
         month_input = month
         day_input = day
         station_code = station
         prefix = year_input + '/' + month_input + '/' + day_input + '/' + station_code + '/'
         geos_bucket = self.s3client.list_objects(Bucket = self.nexrad_bucket_name, Prefix = prefix).get('Contents')
-        logging.info("Printing files from NEXRAD bucket")
-        print("Files available to download from the selected location:")
         file_list = []
         for objects in geos_bucket:
             file_path = objects['Key']
@@ -89,4 +66,34 @@ class AWS_Main:
             file_list.append(file_path[-1])
         return file_list
 
+    def copy_file_to_user_bucket(self, selected_file, file_input, satellite_input):
+        user_bucket = self.s3resource.Bucket(os.environ.get('USER_BUCKET_NAME'))  #define the destination bucket as the user bucker
+        
+        if satellite_input == 'geos18':
+            user_folder = 'GOES18/'
+            file_key = user_folder + file_input
+            url_s3 = 'https://damg-7245-projects.s3.amazonaws.com/' + file_key
+            url_noaa = 'https://noaa-goes18.s3.amazonaws.com/' + selected_file
+            copy_source = {
+                'Bucket': self.geos_bucket_name,
+                'Key': selected_file
+                }
+        
+        elif satellite_input == 'nexrad':
+            user_folder = 'NEXRAD/'
+            file_key = user_folder + file_input
+            url_s3 = 'https://damg-7245-projects.s3.amazonaws.com/' + file_key
+            url_noaa = 'https://noaa-nexrad-level2.s3.amazonaws.com/' + selected_file
+            copy_source = {
+                'Bucket': self.geos_bucket_name,
+                'Key': selected_file
+                }
+        
+        for file in user_bucket.objects.all():
+            if(file.key == file_key):
+                print('Sorry! Cannot copy a file that is already present')
+                print('DOWNLOAD here using URL to already existing file on local S3 bucket: ')
+                return url_s3, url_noaa
 
+        user_bucket.copy(copy_source, file_key)
+        return url_s3, url_noaa
